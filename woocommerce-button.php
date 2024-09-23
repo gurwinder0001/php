afe196f8e03b4b40a0ef43e511a8826dbd8b6c86
gurwinder0001 <gurwindersinghwebwinner@gmail.com>
2024-09-23 10:28:20 +0530

    add to cart button text change

---
+++ woocommerce-button.php
@@ -0,0 +1,5 @@
+<?php// Change add to cart text on single product page
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woocommerce_add_to_cart_button_text_single' ); 
+function woocommerce_add_to_cart_button_text_single() {
+    return __( 'Buy Now', 'woocommerce' ); 
+}
